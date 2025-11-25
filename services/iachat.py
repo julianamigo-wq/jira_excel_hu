a97369122169f70fc87d2de7f962609332dc1684
julianamigo-wq <julian.amigo@testgroup.cl>
2025-11-25 11:02:36 -0300

    Update iachat.py

--- services/iachat.py
+++ services/iachat.py
@@ -3,25 +3,30 @@ from pypdf import PdfReader
 from openai import OpenAI  # Recuerda que estamos usando openrouter para probar de manera gratuita
 
 # ******************************************************************
+issue = ""
 # BASE DEL PROMPT
-textoInicial = """
-A continuación, te proporcionaré la información de una Historia de Usuario.Tu tarea es 
-analizar esta historia, identificar los requisitos clave, y generar una lista de Casos 
-de Prueba (Test Cases) que cubran las funcionalidades descritas, incluyendo escenarios 
-positivos, negativos y de borde cuando sea pertinente.El formato de respuesta debe ser 
-una tabla sencilla (formato básico) con las siguientes columnas para cada caso de 
-prueba:
-ID del Caso Nombre/Descripción del Caso, Pasos a Seguir, Resultado Esperado. 
-Necesito que lo muestres como si fuera un formato csv, y no escribas nada adicional ni 
-sugerencias finales, ni caracteres especiales o adornos de emojis, ni negritas o formatos 
-de texto, solo el resultado de manera sobria. La información es la siguiente: 
+textoInicial = f"""
+A continuación, te proporcionaré la información de una Historia de Usuario. Tu tarea es analizar 
+esta historia, identificar los requisitos clave, y generar una lista de Casos de Prueba (Test Cases) 
+que cubran las funcionalidades descritas, incluyendo escenarios positivos, negativos y de borde 
+cuando sea pertinente.El formato de respuesta debe ser una tabla sencilla con las siguientes columnas 
+para cada caso de prueba, utilizando únicamente la coma (,) como delimitador para separar los campos, 
+sin incluir comas dentro del contenido de las celdas:
+ID del Caso: Debe seguir el formato numero_correlativo_{issue} (ej: 1_{issue}, 2_{issue}, 3_{issue}, etc).
+Nombre/Descripción del Caso
+Pasos a Seguir
+Resultado Esperado.
+Necesito que lo muestres como si fuera un formato CSV puro, y no escribas nada adicional ni sugerencias finales, 
+ni caracteres especiales, adornos de emojis, negritas o formatos de texto. Solo el resultado de manera sobria.
+La información es la siguiente: 
 
 """
 
 # ******************************************************************
-def send_chat(text_doc: str) -> str:
+def send_chat(text_doc: str, name_issue: str) -> str:
     try:
         # Concatenando la extraccion con el textoInicial para generar prompt
+        issue = name_issue
         prompt = textoInicial+text_doc
         
         # generamos la consulta a chatgpt
